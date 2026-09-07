f3a66a05ace4e72f31843b9029b8f8ec9ebb04b0
Giomica <127758919+gnersa@users.noreply.github.com>
2026-09-07 19:10:41 +0700

    Delete page (1).tsx

--- page (1).tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { getCurrentUser } from "@/lib/auth";
-import OnboardingForm from "@/components/OnboardingForm";
-
-export default async function OnboardingPage() {
-  const user = await getCurrentUser();
-  if (!user) redirect("/login");
-  return <OnboardingForm defaultName={user.name} defaultEmail={user.email} />;
-}
